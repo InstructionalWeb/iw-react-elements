0e2a24f7560b08127046fadb8fa758540f2ec1db
Robert Ferguson <rob@rewferguson.com>
2019-01-14 15:31:14 -0600

    project init

---
+++ README.md
@@ -0,0 +1,2 @@
+# iw-react-elements
+Instruction Web React Custom Elements
---
+++ index.js
@@ -0,0 +1,12 @@
+import Section from "./Components/Section";
+import Heading from "./Components/Heading";
+
+/*
+ * The Elements component is a collection of components for spitting out styled markup
+ * Heading component = <h1>, <h2>, <h3>
+ * HR component = <hr>
+ * A component = <a>
+ * and so on
+ */
+
+export { Section, Heading };
